bdaaf5b9228db9be07660cf957c7bfe71d7ab5dd
lees <2102690391@qq.com>
2022-03-23 22:48:38 +0800

    SLAM_3_23

---
+++ include/feature.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#ifndef FEATURE_H
+#define FEATURE_H
+
+#include <memory>
+#include <opencv2/features2d.hpp>
+#include <common_include.h>
+
+namespace lslam{
+struct Frame;
+struct MapPoint;
+
+/**
+ * 2D 特征点
+ * 在三角化之后会被关联一个地图点
+ */
+struct Feature{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    typedef std::shared_ptr<Feature> Ptr;
+
+    // 当对象嵌套时，最好使用weak_ptr 这样可以避免循环引用
+    // 简而言之，Frame 持有了Feature的shared_ptr,那么应该避免Feature
+    // 再持有Frame的shared_Ptr.否则两者相互引用，将导致只能指针无法自动析构
+
+    std::weak_ptr<Frame> frame_;          // 持有该feature的frame
+    cv::KeyPoint position_;               // 2D提取位置
+    std::weak_ptr<MapPoint> map_point_;   //关联地图点
+
+    bool is_outlier=flase;               //是否是异常点
+    bool is_on_left_image_=true;         //标识是否在左图,false为右图
+
+public:
+    Feature(){}
+
+    Feature(std::shared_ptr<Frame> frame, const cv::KeyPoint &kp)
+        :frame_(frame), position_(kp) {}
+
+};
+}   //namespace lslam
+#endif
---
+++ include/frame.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#ifndef LFRAME_H
+#define LFRAME_H
+
+#include<camera.h>
+#include<common_include.h>
+
+namespace lslam{
+    //forward Point
+    struct MapPoint;
+    struct Feature;
+
+/**
+ * 帧
+ * 每一帧分配独立id，关键帧分配关键ID
+ */
+
+struct Frame{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    typedef std::shared_ptr<Frame> Ptr;
+
+    unsigned long id_=0;           //id of this frame
+    unsigned long keyframe_id_=0;  //id of key frame
+    bool is_keyframe_=false;       //是否为关键帧
+    double time_stamp_;            //时间戳，暂时不使用
+    SE3 pose_;                     //Tcw 形式Pose
+    std::mutex pose_mutex_;        //Pose数据锁
+    cv::Mat left_img_,right_img_;  //stero images
+
+    //extracted features in left img
+    std::vector<std::shared_ptr<Feature>> features_left;
+    // corresponding features in right image, set to nullptr if no corresponding
+    std::vector<std::shared_ptr<Feature>> feature_right;
+
+    Frame(){}
+
+    Frame(long id,double time_stamp,const SE3 &pose,const Mat &left,const Mat &right);
+
+    //set an get pose,thread safe
+    SE3 Pose(){
+        std::unique_lock<std::mutex> lck(pose_mutex_);
+        return pose_;
+    }
+
+    void SetPose(const SE3 &pose){
+        std::unique_lock<std::mutex> lck(pose_mutex_);
+        pose_=pose;
+    }
+
+    /// 设置关键帧并分配关键帧id
+    void SetKeyFrame();
+
+    /// 工厂构建模式，分配id
+    static std::shared_ptr<Frame> CreateFrame();
+};
+}
+#endif
